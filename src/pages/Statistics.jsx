dd6aa955e28089c546f92ff2198ccf50b50f7ce5
syedmehedi34 <syedmeehdi34@gmail.com>
2024-11-05 21:41:22 +0600

    statistic page done

--- src/pages/Statistics.jsx
+++ src/pages/Statistics.jsx
@@ -1,12 +1,33 @@
 import { Helmet } from "react-helmet-async";
+import { useLoaderData } from "react-router-dom";
+import {
+  Bar,
+  BarChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 
 const Statistics = () => {
+  const data = useLoaderData();
   return (
-    <div>
+    <div className="w-11/12 mx-auto pb-44">
       <Helmet>
         <title>Statistics -Gadget Heaven</title>
       </Helmet>
-      <p>Statistics page</p>
+
+      <div className="flex items-center justify-center mx-auto">
+        <ResponsiveContainer width="70%" height={300}>
+          <p className="py-12 text-titleOB text-2xl font-bold">Statistics</p>
+          <BarChart data={data}>
+            <XAxis dataKey="product_name" />
+            <YAxis />
+            <Tooltip />
+            <Bar dataKey="price" barSize={30} fill="#8884d8" />
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   );
 };
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -34,6 +34,7 @@ const routes = createBrowserRouter([
       },
       {
         path: "/statistics",
+        loader: () => fetch("../products.json"),
         element: <Statistics></Statistics>,
       },
       {
